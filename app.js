7dc549b8f2270f3c905abb07dc373b9ebfbf2f9f
rciup <rciupek0@gmail.com>
2023-01-26 04:02:28 +0100

    duży comit

--- app.js
+++ app.js
@@ -12,14 +12,21 @@ const karta = {
 		return {
 			cards: [],
             deck: [],
+            decks: [],
             classes: info.classes,
-            areCardsVisible: false
+            className: undefined,
+            areCardsVisible: false,
+            isDecksMenuVisible: false
 		}
 	},
 	methods: {
-		getCards(className) {
+		getCards(mana) {
             this.areCardsVisible = true
-			fetch('https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/classes/' + className, options)
+            let url= 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/classes/' + this.className
+            if(mana!== undefined){
+                url += '?cost='+mana
+            }
+			fetch( url , options)
 			.then(response => response.json())
 			.then(response => response.filter(card => card.hasOwnProperty('img') && card.hasOwnProperty('type') && card.type != "Hero" && card.type != "Hero Power"))
 			.then(response => {
@@ -30,9 +37,14 @@ const karta = {
             this.classes = []
             
 		},
+        choseClass(className){
+            this.className=className
+            this.getCards()
+        },
         backToClasses(){
             this.areCardsVisible = false
             this.cards = []
+            this.deck = []
             this.classes = info.classes
         },
         addCardToDeck(card){
@@ -57,28 +69,24 @@ const karta = {
         },
         generateName()
         {
-            return ("New deck " + (this.getDecks().length+1))
+            return ("New deck " + (this.decks.length+1))
         },
         saveDeck(name="default"){
             //DONE//TODO trzeba zrobić ekran w którym będziemy podawać nazwę deku, może to być mały popup 
-            //TODO trzeba zrobić ekran do przeglądania nazw istniejących już deków. Po wybraniu nazwy powinien się załadować.
+            //DONE//TODO trzeba zrobić ekran do przeglądania nazw istniejących już deków. Po wybraniu nazwy powinien się załadować.
             //DONE//TODO walidacje jakiegoś dodawania, liczby kart 30 itp -------blokada do 30 i max 2 duplikaty, ale problem bo 2 te samy karty są wyswietlane na liscie dostepnych 
-            //TODO Button do usunięcia karty z decku bo na razie jest to zrobione na luzie
+            //DONE//TODO Button do usunięcia karty z decku bo na razie jest to zrobione na luzie
             //TODO dodatkowe ekran z szczegółowym wyświetleniem informacji na temat karty
             //TODO usuwanie chyba usuwa duzo naraz nwm xd
-            let decks = this.getDecks()
+            //TODO można zrobić wyszukiwanie poprzez nazwę, ale nie trzeba
+            this.getDecks()
             let DeckName = prompt("Please enter deck name",this.generateName());
-            name = DeckName;
-            console.log(DeckName)
-            
-            
-            console.log(decks)
-            
-            decks.push({
+            name = DeckName;            
+            this.decks.push({
                 "name": name,
                 "deck": this.deck
             })
-            localStorage.decks = JSON.stringify(decks)
+            localStorage.decks = JSON.stringify(this.decks)
         },
         removeCardFromDeck(card){
             for( let i = 0; i < this.deck.length; i++){                     
@@ -88,27 +96,57 @@ const karta = {
             }
         },
         getDecks(){
-            let decks =[]
             if (localStorage.decks && localStorage.decks!=[]){
-                decks = JSON.parse(localStorage.decks)
+                this.decks = JSON.parse(localStorage.decks)
+            }         
+        },
+        showDecksMenu(){
+            this.getDecks()
+            this.isDecksMenuVisible = true
+        },
+        closeDecksMenu(){
+            this.isDecksMenuVisible = false
+            this.decks = []
+        },
+        loadDeck(deck){
+            this.deck=deck.deck
+        },
+        deleteDeck(deck){
+            for( let i = 0; i < this.decks.length; i++){                     
+                if ( this.decks[i] === deck) { 
+                    this.decks.splice(i, 1); 
+                }
             }
-            console.log("dek z local storage")
-            console.log(decks)
-            
-            return decks
+            localStorage.decks = JSON.stringify(this.decks)
         }
 	},
 	template: `
     <div class="top">
         <img class="top-logo" src="https://d2q63o9r0h0ohi.cloudfront.net/images/npe/header/rose_icon_header-2c41dc61329a7d80e3f277baf149ac1fa7f6ba9b0095cbbec5b39ab21b164bb1c5909a06b6880f75e6d99c15787e4b0f246dd9d78ab8682c3bd5e0f068b0eb63.png">
-        <button class="back-button" v-show="areCardsVisible" @click="backToClasses"> Back </button>
+        <div v-show="areCardsVisible" @click="backToClasses" class="hs-wrapper gold top-button">
+            <a class="hs-button gold">
+                <span class="hs-border gold">
+                    <span class="hs-text gold">
+                        Back
+                    </span>
+                </span>
+            </a>
+        </div>
         <h1>Hearthstone Deck Builder</h1>
-        <button class="back-button" v-show="areCardsVisible" @click="backToClasses"> Decks </button>
+        <div v-show="areCardsVisible" @click="showDecksMenu" class="hs-wrapper gold top-button">
+            <a class="hs-button gold">
+                <span class="hs-border gold">
+                    <span class="hs-text gold">
+                        Decks
+                    </span>
+                </span>
+            </a>
+        </div>
     </div>
        
     <div v-show="!areCardsVisible" class="heroes">
     <h2>Choose hero class: </h2> 
-        <div class="hero-item" v-for="c in classes" @click="getCards(c.name)">
+        <div class="hero-item" v-for="c in classes" @click="choseClass(c.name)">
             <img class="hero-image" v-bind:src="c.img">
             <img class="hero-image2" src="https://d2q63o9r0h0ohi.cloudfront.net/images/deckbuilder/frame_class_portrait_hover-0fe18e7eeb22e9f78e7226fb438e6a8d12b78946b58ae5d735d7869714c62848a7772c8bc424c54063d749bdb6c96d7c83e1617694c5f79b15cf1656651551c7.png">
         </div>
@@ -117,7 +155,15 @@ const karta = {
     
 
     <div v-show="areCardsVisible" class="middle">
+
         <div class= "left-panel">
+        <div id="search-bar">
+            <div id="mana-point" v-for="n in 11" @click="getCards(n-1)">
+                <img src="https://d2q63o9r0h0ohi.cloudfront.net/images/card-gallery/icon_mana_68x68_@2x-b6ecc2a45a3440ac6cba07297306fbb64d9a224bc12a281224a091aaf92a73da2d0036e6eed920f74a957660354bfafe7831fc291aad49867d04b427737c6e3a.png">
+                <p>{{n-1}}</p>
+            </div>
+        </div>
+
             <div class="element" v-for="c in cards" @click="addCardToDeck(c)">
                 <img v-bind:src="c.img">
             </div>
@@ -125,25 +171,41 @@ const karta = {
 
         <div class= "right-panel">
             <div class="deck-menu">
-                <div v-for="d in deck" class="list-elem" >
+                <div v-for="d in deck" class="list-elem options-on-hover" >
                     <p class="list-elem-cost">{{ d.cost }}</p> 
                     <p class="list-elem-name">{{ d.name }}</p> 
                     <img src="https://d2q63o9r0h0ohi.cloudfront.net/images/decklist/card_list_left-c1a0e1834e1ff7351c0f929282db3cfb44293b366a4433ffdfc0c097ebd01bbc2951caaf3bd03379be1f30907a58fcaf7c8a1f2ea60805bcec1519e216ee0d54.png">            
                     <img src="https://d2q63o9r0h0ohi.cloudfront.net/images/decklist/card_list_middle-356a94f18c483c86b6cc02773fda2d345259b428932db41718e7be1873e8a737cd2ae5c58ff7ded8144f81ecb7748f1b1e2e50ad155c993b40d86fa1c3a825ac.png">
                     <img src="https://d2q63o9r0h0ohi.cloudfront.net/images/decklist/card_list_middle-356a94f18c483c86b6cc02773fda2d345259b428932db41718e7be1873e8a737cd2ae5c58ff7ded8144f81ecb7748f1b1e2e50ad155c993b40d86fa1c3a825ac.png">         
                     <img src="https://d2q63o9r0h0ohi.cloudfront.net/images/decklist/card_list_middle-356a94f18c483c86b6cc02773fda2d345259b428932db41718e7be1873e8a737cd2ae5c58ff7ded8144f81ecb7748f1b1e2e50ad155c993b40d86fa1c3a825ac.png">         
-                    <img @click="removeCardFromDeck(d)" src="https://d2q63o9r0h0ohi.cloudfront.net/images/decklist/card_list_right-cc4f9b7f9f64ddf99755527449897fa91a5d699b6aa8f12ad79bfdb04113c49d92a4d7c9c1689b1373459393410b19c29da235465833f5c51f0b026f3737eb2b.png">        
+                    <img src="https://d2q63o9r0h0ohi.cloudfront.net/images/decklist/card_list_right-cc4f9b7f9f64ddf99755527449897fa91a5d699b6aa8f12ad79bfdb04113c49d92a4d7c9c1689b1373459393410b19c29da235465833f5c51f0b026f3737eb2b.png">
+                    <img @click="removeCardFromDeck(d)" class="delete-on-hover" src="https://cdn-icons-png.flaticon.com/512/7159/7159094.png">        
                 </div>
-                <button @click="saveDeck('nazwaDecku')">Save deck</button>
-                <div class="hs-wrapper gold">
-                <a class="hs-button gold" href="">
-                    <span class="hs-border gold">
-                        <span class="hs-text gold">
-                            Play Now
+                <div id="save-button" @click="saveDeck('nazwaDecku')" class="hs-wrapper gold">
+                    <a class="hs-button gold">
+                        <span class="hs-border gold">
+                            <span class="hs-text gold">
+                                Save deck
+                            </span>
                         </span>
-                    </span>
-                </a>
-		    </div>
+                    </a>
+		        </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div v-show="isDecksMenuVisible" class="window-menu" >
+        <div id="window-wrapper">
+            <button class="close-button" @click="closeDecksMenu"></button>
+            <div class="list">
+                <div class="list-elem" id="decks-item" v-for="d in decks" @click="loadDeck(d)">
+                    <div class="p-and-button">
+                        <p>{{ d.name }}</p> 
+                        <button class="delete-button" @click="deleteDeck(d)"></button> 
+                    </div>
+                </div>
             </div>
 
         </div>
